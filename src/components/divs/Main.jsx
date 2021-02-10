df65d69564e8438accea930a369119fb4745cd52
Ranielli Montagna <182746@upf.br>
2021-02-10 16:58:43 -0300

    Add Typewriter effect in main div

--- src/components/divs/Main.jsx
+++ src/components/divs/Main.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import "./Main.css";
 
-import myPhoto from "../../images/myPhoto.jpg"
-import { Image } from 'react-bootstrap';
+import myPhoto from "../../images/myPhoto.jpg";
+import { Image } from "react-bootstrap";
+import Typewriter from "typewriter-effect";
 
 function Main() {
   return (
     <div className="main">
       <Image src={myPhoto} roundedCircle />
-      <h1>Olá! Esse sou eu, bem-vindos ao meu site.</h1>
-      <h2>Aqui você pode conhecer um pouco mais sobre mim e o que eu faço.</h2>
-
+      <Typewriter
+        className="typewriter"
+        options={{
+          strings: [
+            "Olá! Esse sou eu, bem-vindos ao meu site.",
+            "Aqui você pode conhecer um pouco mais sobre mim e o que eu faço.",
+          ],
+          autoStart: true,
+          loop: true,
+          delay: 50,
+          pauseFor: 3000,
+          deleteSpeed: 20,
+        }}
+      />
     </div>
   );
 }
