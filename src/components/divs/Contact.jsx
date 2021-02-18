4bbdeb6be865a226d06f3f2394ceb7d739f355c5
Ranielli Montagna <76871529+RaniMontagna@users.noreply.github.com>
2021-02-18 16:16:43 -0300

    Add effects in contact div and fix dropdown

--- src/components/divs/Contact.jsx
+++ src/components/divs/Contact.jsx
@@ -5,41 +5,50 @@ function Contact() {
   return (
     <div id="contact" className="contact">
       <div className="title">
-        <h1>
+        <h1 data-aos="fade-down">
           Entre <strong> em contato </strong>
         </h1>
       </div>
       <div className="row">
         <div className="form">
-        <h2> Envie uma mensagem:</h2>
+          <h2 data-aos="fade-right"> Envie uma mensagem:</h2>
           <form className="formulario">
             <input
+              data-aos="fade"
               type="text"
               placeholder="Seu nome"
               id="name"
               className="name"
             />
             <input
+              data-aos="fade"
               type="text"
               placeholder="Seu email"
               id="email"
               className="email"
             />
-            <textarea rows="10" cols="30" placeholder="Sua mensagem" id="message" className="message"/>
+            <textarea
+              data-aos="fade"
+              rows="10"
+              cols="30"
+              placeholder="Sua mensagem"
+              id="message"
+              className="message"
+            />
 
-            <button type="submit" className="submit">
+            <button data-aos="fade-left" type="submit" className="submit">
               Enviar Mensagem
             </button>
           </form>
         </div>
         <div className="contactDetails">
-          <h2> Meus detalhes de contato:</h2>
-          <h6>EMAIL</h6>
-          <p>raniellimontagna@hotmail.com</p>
-          <h6>TELEFONE</h6>
-          <p>+55 54 99979-0871</p>
-          <h6>ENDEREÇO</h6>
-          <p>Paraí, RS - Linha Navegantes, 2035</p>
+          <h2 data-aos="fade-right"> Meus detalhes de contato:</h2>
+          <h6 data-aos="fade-down">EMAIL</h6>
+          <p data-aos="fade-down">raniellimontagna@hotmail.com</p>
+          <h6 data-aos="fade-down">TELEFONE</h6>
+          <p data-aos="fade-down">+55 54 99979-0871</p>
+          <h6 data-aos="fade-down">ENDEREÇO</h6>
+          <p data-aos="fade-down">Paraí, RS - Linha Navegantes, 2035</p>
         </div>
       </div>
     </div>
