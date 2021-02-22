51209f21ba8f19a29a48e7fb7af32dbdbb3c07ab
Ranielli Montagna <76871529+RaniMontagna@users.noreply.github.com>
2021-02-22 17:19:15 -0300

    Rework cards

--- src/components/divs/Programming.jsx
+++ src/components/divs/Programming.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, Card, CardGroup } from "react-bootstrap";
+import ImgMediaCard from "../layout/Card";
 import "./Programming.css";
 
 function Programming() {
@@ -8,78 +9,34 @@ function Programming() {
       <h1 data-aos="zoom-in">Programação</h1>
       <h2 data-aos="zoom-in">Minhas Linguagens Preferidas:</h2>
       <div className="cards">
-        <CardGroup>
-          <Card data-aos="fade-up" className="card" style={{ width: "18rem" }}>
-            <Card.Img
-              variant="top"
-              src="/images/programming/reactJS.webp"
-            />
-            <Card.Body>
-              <Card.Title>ReactJS</Card.Title>
-              <Card.Text>
-                O React é uma biblioteca JavaScript de código aberto com foco em
-                criar interfaces de usuário em páginas web. É mantido pelo
-                Facebook, Instagram, outras empresas e uma comunidade de
-                desenvolvedores individuais. É utilizado nos sites da Netflix,
-                Imgur, Airbnb, Walmart e outros.
-              </Card.Text>
-              <Button
-                href="https://pt-br.reactjs.org"
-                target="_blank"
-                variant="dark"
-              >
-                Saiba Mais
-              </Button>
-            </Card.Body>
-          </Card>
-
-          <Card data-aos="fade-up" className="card" style={{ width: "18rem" }}>
-            <Card.Img
-              variant="top"
-              src="/images/programming/postgreSQL.webp"
-            />
-            <Card.Body>
-              <Card.Title>PostgreSQL</Card.Title>
-              <Card.Text>
-                PostgreSQL é um sistema de banco de dados poderoso e em código
-                aberto para Windows, Mac e Linux. Ele possui mais de 15 anos de
-                desenvolvimento ativo e uma arquitetura que ganhou uma forte
-                reputação devido a sua estabilidade e integridade de dados.
-              </Card.Text>
-              <Button
-                href="https://www.postgresql.org"
-                target="_blank"
-                variant="dark"
-              >
-                Saiba Mais
-              </Button>
-            </Card.Body>
-          </Card>
-
-          <Card data-aos="fade-up" className="card" style={{ width: "18rem" }}>
-            <Card.Img
-              variant="top"
-              src="/images/programming/css.webp"
-            />
-            <Card.Body>
-              <Card.Title>CSS</Card.Title>
-              <Card.Text>
-                O CSS é uma linguagem de folha de estilo composta por “camadas”,
-                criado com o propósito de estilizar as páginas HTML, ou seja,
-                definir a aparência das páginas, para deixá-las visualmente mais
-                bonitas e agradáveis, podendo alterar a fonte, cor,
-                posicionamento dos elementos, layout e muito mais.
-              </Card.Text>
-              <Button
-                href="https://developer.mozilla.org/pt-BR/docs/Web/CSS"
-                target="_blank"
-                variant="dark"
-              >
-                Saiba Mais
-              </Button>
-            </Card.Body>
-          </Card>
-        </CardGroup>
+        <ImgMediaCard
+          image="/images/programming/reactJS.webp"
+          title="ReactJS"
+          text="O React é uma biblioteca JavaScript de código aberto com foco em
+          criar interfaces de usuário em páginas web. É mantido pelo
+          Facebook, Instagram, outras empresas e uma comunidade de
+          desenvolvedores individuais. É utilizado nos sites da Netflix,
+          Imgur, Airbnb, Walmart e outros."
+          link="https://pt-br.reactjs.org"
+        />
+        <ImgMediaCard
+          image="/images/programming/postgreSQL.webp"
+          title="PostgreSQL"
+          text="PostgreSQL é um sistema de banco de dados poderoso e em código
+          aberto para Windows, Mac e Linux. Ele possui mais de 15 anos de
+          desenvolvimento ativo e uma arquitetura que ganhou uma forte
+          reputação devido a sua estabilidade e integridade de dados."
+          link="https://www.postgresql.org"
+        />
+        <ImgMediaCard
+          image="/images/programming/css.webp"
+          title="CSS"
+          text="O CSS é uma linguagem de folha de estilo composta por “camadas”,
+          criado com o propósito de estilizar as páginas HTML, ou seja,
+          definir a aparência das páginas, para deixá-las visualmente mais
+          bonitas e agradáveis, podendo alterar a fonte, cor, layout e muito mais."
+          link="https://developer.mozilla.org/pt-BR/docs/Web/CSS"
+        />
       </div>
     </div>
   );
---
+++ src/components/layout/Card.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+    marginBottom: 10,
+    marginLeft: 5,
+    marginRight: 5,
+  },
+});
+
+export default function ImgMediaCard(props) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root} data-aos="fade-up">
+      <CardActionArea href={props.link} target="_blank" className="card">
+        <CardMedia
+          component="img"
+          alt={props.title}
+          height="180"
+          image={props.image}
+          title={props.title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.text}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
