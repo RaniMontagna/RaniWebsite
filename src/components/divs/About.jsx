148665723a108fb5d7075590c5788ba717c7f24a
Ranielli Montagna <76871529+RaniMontagna@users.noreply.github.com>
2021-06-30 08:34:41 -0300

    Refaz parte do texto sobre mim

--- src/components/divs/About.jsx
+++ src/components/divs/About.jsx
@@ -17,10 +17,10 @@ function About() {
           pequeno e pretendo melhorar a cada dia mais. Além disso, tenho
           experiências em outros áreas dentro da tecnologia como por exemplo,
           Designer Gráfico e Projetista de Interiores. Esses e outros graças à
-          minha passagem de 7 em cursos de informática. No momento estou
-          trabalhando como Projetista de Móveis, porém meu objetivo é com que ao
-          passar dos semestres da faculdade eu consiga entrar no mercado de
-          trabalho de softwares ou algo semelhante.
+          minha passagem de 7 em cursos de informática. Já trabalhei como
+          Projetista de Móveis, porém hoje estou no mercado de desenvolvimento
+          de software dentro da empresa <strong>SBSistemas</strong> de Marau,
+          sou programador front-end!
         </p>
       </div>
       <SvgDivLayoutBottom />
