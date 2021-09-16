9c99438adc66db7ef4f7ee923f81c5d4f6286a53
Ranielli Montagna <ranielli@sbsistemas.com.br>
2021-09-16 13:01:54 -0300

    refactor: adiciona altura e largura a retângulo svg

--- src/components/layout/svgDivLayoutBottom.jsx
+++ src/components/layout/svgDivLayoutBottom.jsx
@@ -6,7 +6,11 @@ import Rectangle from "../../images/layout/Rectangle.png";
 function SvgDivLayoutBottom() {
   return (
     <div className="svg">
-      <img src={Rectangle} alt="RectangleDiv" />
+      <img
+        src={Rectangle}
+        alt="RectangleDiv"
+        style={{ width: "100%", height: "15px" }}
+      />
     </div>
   );
 }
